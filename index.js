be6ffee8f8d043e74d4969680250eb61a88e6db2
nyabangWech <158462142+nyabangWech@users.noreply.github.com>
2024-03-18 21:03:25 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,70 @@
+
+let x=5;
+let y=10;
+let z=x+y;
+console.log(z);
+let w=5;
+let q=20;
+let A=w*q;
+console.log(A);
+let a=3;
+let X=(100+50)* a;
+console.log(x);
+let b=10;
+b +=5;
+console.log(b);
+let text1="nyabang"
+let text2="wech"
+let text3=text1+" "+text2;
+console.log(text3);
+let text4="what a very ";
+let text5="nice day";
+let text6=text4+" "+text5;
+console.log(text6);
+let v=5+5;
+let u="5"+5;
+let c="Hello"+5;
+console.log(c);
+let strictlyEqualTo=y ===X;
+console.log(strictlyEqualTo);
+let stritlyNotEqualto=y ===X;
+console.log(stritlyNotEqualto);
+let word="Abcdefghijklmnopqrstuvwxyz";
+let length=word.length;
+console.log(length);
+//let text="we are the so-called vikings from the north";
+let text="HELLO WORLD"
+let char=text.charAt(0);
+console.log(char);
+let text7="mummy";
+let chars=text7.charCodeAt(0)
+console.log(chars);
+//nlet tex8="Heloo world";
+//ext8[0]="A"
+//console .log(text8);
+let fruit="Apple,banana,mango";
+let part=fruit.slice(7,13);
+console.log(part);
+let str="pineapple,grapes,mango"
+let parts=str.substring(7,13);
+console.log(parts);
+let text9="hello,monasha";
+let text10= text9.toLowerCase();
+console.log(text10);
+let text11="we will make it";
+let text12=text11.toUpperCase();
+let text13="NATASHA";
+let text14=text13.trimEnd();
+console.log(text14);
+let number=7;
+let text15=number.toString
+let  padded=text.padEnd(4,"0");
+console.log(padded);
+let text16="world";
+let result=text16.repeat(4);
+console.log(result);
+let text17="please visit microsoft"
+let newtext=text17.replace("microsoft","w3schools");
+console.log(newtext);
+
+
